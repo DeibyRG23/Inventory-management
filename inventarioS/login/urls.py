a0993145c54b5576c687cdb6405588879d8e34e6
DeibyR <deiby_2304@hotmail.com>
2025-08-15 22:03:40 -0500

    Login funcional con vistas hechas con bootstrap y responsivo

---
+++ inventarioS/login/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns=[
+    path('',inicio_sesion,name="home"),
+    path('logout',cerrar_sesion,name="cerrar_sesion"),
+    path('principal',main,name="main"),
+]
--- inventarioS/login/views.py
+++ inventarioS/login/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
 
 # Create your views here.
+def inicio_sesion(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("main")
+        else:
+            messages.error(request, "Usuario o contraseña incorrectos")
+
+    return render(request, "login/login.html")
+
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect("home")
+
+
+def main(request):
+    return render(request, "main.html")
