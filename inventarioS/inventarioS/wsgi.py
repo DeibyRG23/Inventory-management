2145355387fdbcf56a3b8b6efba55abf562ffcd9
Deiby Rodriguez <85973574+DeibyRG23@users.noreply.github.com>
2025-10-07 00:21:40 -0500

    Update wsgi.py

--- inventarioS/inventarioS/wsgi.py
+++ inventarioS/inventarioS/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventarioS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventarioS.inventarioS.settings')
 
 application = get_wsgi_application()
